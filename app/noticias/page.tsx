f641bb58e8f73af913b10fbe0351a472d37b6290
comprasrocoto-dotcom <comprasrocoto@gmail.com>
2026-07-21 19:26:44 -0500

    Add NoticiasPage with ComingSoon component

---
+++ app/noticias/page.tsx
@@ -0,0 +1,10 @@
+import { ComingSoon } from "@/components/coming-soon";
+
+export default function NoticiasPage() {
+  return (
+    <ComingSoon
+      title="Noticias"
+      description="Las novedades y comunicados internos de la compania apareceran aqui muy pronto."
+    />
+  );
+}
